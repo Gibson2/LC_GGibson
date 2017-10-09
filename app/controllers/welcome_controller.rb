74d51c45a6adcb7d6a95d235311518477c8c51d1
Guillermo <gibsonguillermoarturo@gmail.com>
2017-10-09 10:41:20 -0300

    Add Controller of Welcome

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def news
+  end
+
+  def contact
+  end
+
+  def service
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/news'
+
+  get 'welcome/contact'
+
+  get 'welcome/service'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
