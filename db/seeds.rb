20561bcbf14560e16fd0affd41b9cdbbec59c4f6
Guillermo <gibsonguillermoarturo@gmail.com>
2017-11-01 17:23:29 -0300

    add kaminari in the gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+Comment.destroy_all
+User.destroy_all
+
+#Providencias de Chile
+pro = Province.create
+pro.cod_country = CL
+
+pro([{name: "Los Lagos", cod_province: "LL"},
+	{name: "Valparaiso", cod_province: "VS"},
+	{name: "Region Metropolitana de Santiago", cod_province: "RM"},
+	{name: "Biobio", cod_province: "BI"},
+	{name: "Atacama", cod_province: "AT"},
+	{name: "Tarapaca", cod_province: "TA"},
+	{name: "Aysen", cod_province: "AI"},
+	{name: "Maule", cod_province: "ML"},
+	{name: "Coquimbo", cod_province: "CO"},
+	{name: "Araucania", cod_province: "AR"},
+	{name: "Antofagasta", cod_province: "AN"},
+	{name: "Arica y Parinacota", cod_province: "AP"},
+	{name: "Magallanes", cod_province: "MA"},
+	{name: "Caleta Patillos", cod_province: "CAL"},
+	{name: "Libertador General Bernardo O'Higgins", cod_province: "LI"},
+	{name: "Los Rios", cod_province: "LR"},
+	{name: "Punta Padrones", cod_province: "PPD"},
+	{name: "Punta Patache", cod_province: "PPC"},
+	])
