7386bfedfe81281ecbb4b428ecd0313785b921ad
Guillermo <gibsonguillermoarturo@gmail.com>
2017-11-01 01:43:27 -0300

    configure the send of emails

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,25 @@ module Llevamecontigo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    if Rails.env.production?
+    	#Production
+    	config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    else
+    	#Develoment
+    	config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    end
+	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.default :charset => "utf-8"
+
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+	  address:              'smtp.gmail.com',
+	  port:                 587,
+	  authentication:       'plain',
+	  domain:               'gmail.com',
+	  user_name:            ENV['gmail_user'],
+	  password:             ENV['gmail_pass'],
+	  enable_starttls_auto: true  }
   end
 end
